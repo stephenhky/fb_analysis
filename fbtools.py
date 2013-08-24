a6ecc2cfc93ea3781244a7f332ed9944ec5150e9
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-08-24 18:00:32 -0400

    Update fbtools.py

--- fbtools.py
+++ fbtools.py
@@ -6,6 +6,7 @@ Created on Sat Aug 24 16:22:22 2013
 """
 
 import urllib2, urllib
+import json
 
 fql_query_prefix = 'https://graph.facebook.com/fql?'
 
@@ -17,4 +18,22 @@ def FQLquery(fql_query, access_token):
                                          'access_token': access_token})
     url = fql_query_prefix + quoted_query
     return urllib2.urlopen(url).read()
+
+def getName(uid, access_token):
+    query = 'select name from user where uid='+str(uid)
+    jfriend_data = FQLquery(query, access_token)
+    friend_data = json.loads(jfriend_data)
+    return friend_data['data'][0]['name']
+
+def getFriendList(uid, access_token):
+    fql_query = 'select uid2 from friend where uid1='+str(uid)
+    jfriendsuid = FQLquery(fql_query, access_token)
+    friendsuid = json.loads(jfriendsuid)['data']
     
+    friends = []
+    for frienduid in friendsuid:
+        uid = frienduid['uid2']
+        #friends.append({'uid': uid, 'name': getName(uid, access_token)})
+        friends.append({'uid': uid})
+        
+    return friends
