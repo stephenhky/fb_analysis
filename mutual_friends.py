b190052c8b2b7a05e36ba92e8473190ff963d914
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-08-27 17:39:56 -0400

    Update mutual_friends.py

--- mutual_friends.py
+++ mutual_friends.py
@@ -13,9 +13,9 @@ from string import Template
 class MutualFriendsChecker(frdholder.FriendListHolder):
     def __init__(self, selfuid, access_token):
         frdholder.FriendListHolder.__init__(self, selfuid, access_token)
-        self.mutualfrd_pairs = self.get_mutual_friends(self.selfuid)
+        self.mutualfrd_pairs = self.get_mutual_friends()
         
-    def get_mutual_friends(self, uid):
+    def get_mutual_friends_old(self, uid):
         fql_templ = ' SELECT uid1, uid2 FROM friend '+\
                     ' WHERE uid1 IN '+\
                     '(SELECT uid2 FROM friend WHERE uid1= $personuid)' +\
@@ -24,3 +24,31 @@ class MutualFriendsChecker(frdholder.FriendListHolder):
         fql_query = Template(fql_templ).substitute(personuid=uid)
         jresults = fbtools.FQLquery(fql_query, self.access_token)
         return json.loads(jresults)['data']
+        
+    def get_mutual_friends(self):
+        fql_templ = 'select uid1, uid2 from friend '+\
+                    'where uid1=$uid1'+\
+                    ' and uid2 in (select uid2 from friend where uid1=$selfuid)'
+        mutualfrd_pairs = []
+        for uid in self.friend_uids:
+            print 'Finding you mutual friends with '+uid
+            fql_query = Template(fql_templ).substitute(uid1=uid,
+                                                       selfuid=self.selfuid)
+            jresults = fbtools.FQLquery(fql_query, self.access_token)
+            for json_item in json.loads(jresults)['data']:
+                mutualfrd_pairs.append(json_item)
+        return mutualfrd_pairs
+    
+    def count_mutual_friends(self):
+        friend_mfcount_dict = {}
+        for pair in self.mutualfrd_pairs:
+            friend_uid = pair['uid1']
+            if friend_mfcount_dict.has_key(friend_uid):
+                friend_mfcount_dict[friend_uid] += 1
+            else:
+                friend_mfcount_dict[friend_uid] = 1
+        friend_mfcounts = []
+        for uid in friend_mfcount_dict.keys():
+            friend_mfcounts.append({'uid': uid,
+                                    'mutualfriends_count': friend_mfcount_dict[uid]})
+        return friend_mfcounts
