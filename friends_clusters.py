4585f1a0f69ebdf5273b50d91752ddcb6b057c4d
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-09-05 15:36:01 -0400

    add object serialization

--- friends_clusters.py
+++ friends_clusters.py
@@ -7,11 +7,12 @@ Created on Thu Sep  5 14:41:13 2013
 
 import mutual_friends as mf
 import resistancedist as resist
+import pickle
 
 frdpairdicttotuple = lambda item: (item[u'uid1'], item[u'uid2'])
 
 class FriendResistanceDistancesWrapper:
-    def __init__(self, selfuid=None, access_token=None):
+    def __init__(self, selfuid=None, access_token=None, pickle_prefix=None):
         if selfuid != None and access_token != None:
             self.selfuid = selfuid
             self.access_token = access_token
@@ -19,6 +20,12 @@ class FriendResistanceDistancesWrapper:
             self.mfwrapper = mf.MutualFriendsChecker(selfuid, access_token)
             self.friends = self.mfwrapper.friend_uids
             self.mutfrd_pairs_dict = self.mfwrapper.get_all_mutual_friends_pairs()
+            self.getResistanceMatrix()
+        elif pickle_prefix != None:
+            self.friends = pickle.load(open(pickle_prefix+'_friends.fb', 'rb'))
+            self.mutfrd_pairs_dict = pickle.load(open(pickle_prefix+'_mutfrd.fb',
+                                                      'rb'))
+            self.getResistanceMatrix()
             
     def getResistanceMatrix(self):
         pairs = []
@@ -28,6 +35,6 @@ class FriendResistanceDistancesWrapper:
                                                          edges=pairs)
     
     def getResistance(self, uid1, uid2):
-        idx1 = self.resistdist.nodesIdx[uid1]
-        idx2 = self.resistdist.nodesIdx[uid2]
-        return self.resistdist.getResistance(idx1, idx2)
+        #idx1 = self.resistdist.nodesIdx[uid1]
+        #idx2 = self.resistdist.nodesIdx[uid2]
+        return self.resistdist.getResistance(uid1, uid2)
